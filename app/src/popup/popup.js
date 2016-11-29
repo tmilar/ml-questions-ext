66ae67d6c855362b1f6f5ac0fbe794383a6119f6
tmilar <tomasmilar4@gmail.com>
2016-11-29 05:33:25 -0300

    Add jquery waitMe plugin library

--- app/src/popup/popup.js
+++ app/src/popup/popup.js
@@ -1,8 +1,28 @@
 'use strict';
 
+var waitMe = {
+  $el: '.container',
+  start: function (options) {
+    waitMe.$el = (options && options.selector) ? options.selector : waitMe.$el;
+    $(waitMe.$el).waitMe(_.extend({}, {
+      effect: 'orbit',
+      bg: 'rgba(245,245,245,0.7)',
+      color: '#000',
+      maxSize: '120',
+      onClose: function () {
+        console.log('[waitme] hide ' + waitMe.$el);
+      }
+    }, options));
+  },
+  setText: function (text) {
+    $(".waitMe_text").html(text);
+  },
+  stop: function () {
+    $(waitMe.$el).waitMe('hide');
+  }
+};
+
 $(document).on("ready", function () {
 
-  setTimeout(function () {
-    location.href = location.origin + "/src/popup/pregus.html";
-  }, 5);
+  waitMe.start({selector: '.container', text: "iniciando..."});
 });
