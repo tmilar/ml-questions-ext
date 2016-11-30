caccd3c5374d864d3cbf1578ee8a82c299481c7a
tmilar <tomasmilar4@gmail.com>
2016-11-30 00:20:05 -0300

    Autoreformat .js code to indent of 4 spaces

--- app/src/popup/popup.js
+++ app/src/popup/popup.js
@@ -1,57 +1,56 @@
 'use strict';
 
 var waitMe = {
-  $el: '.container',
-  start: function (options) {
-    waitMe.$el = (options && options.selector) ? options.selector : waitMe.$el;
-    $(waitMe.$el).waitMe(_.extend({}, {
-      effect: 'orbit',
-      bg: 'rgba(245,245,245,0.7)',
-      color: '#000',
-      maxSize: '120',
-      onClose: function () {
-        console.log('[waitme] hide ' + waitMe.$el);
-      }
-    }, options));
-  },
-  setText: function (text) {
-    $(".waitMe_text").html(text);
-  },
-  stop: function () {
-    $(waitMe.$el).waitMe('hide');
-  }
+    $el: '.container',
+    start: function start(options) {
+        waitMe.$el = options && options.selector ? options.selector : waitMe.$el;
+        $(waitMe.$el).waitMe(_.extend({}, {
+            effect: 'orbit',
+            bg: 'rgba(245,245,245,0.7)',
+            color: '#000',
+            maxSize: '120',
+            onClose: function onClose() {
+                console.log('[waitme] hide ' + waitMe.$el);
+            }
+        }, options));
+    },
+    setText: function setText(text) {
+        $(".waitMe_text").html(text);
+    },
+    stop: function stop() {
+        $(waitMe.$el).waitMe('hide');
+    }
 };
 
-
 function startLogin() {
-  waitMe.start({selector: '.container', text: "iniciando sesion..."});
+    waitMe.start({selector: '.container', text: "iniciando sesion..."});
 
-  return new Promise(window.oauth2.start.bind(window.oauth2))
-    .then(function () {
-      console.log("Login good!");
-    })
-    .then(function () {
-      // TODO handle user_id response data?
-      return Promise.resolve($.ajax({
-        type: 'GET',
-        url: 'https://api.mercadolibre.com/users/me?access_token=' + window.oauth2.getAuth().token,
-        success: function (data) {
-          console.log("login success! ", data);
-        }
-      }))
-    })
-    .then(loginSuccess)
-    .catch(function (err) {
-      console.error("Login bad: " + err.stack);
-    })
-    .finally(function () {
-      waitMe.stop();
-    });
+    return new Promise(window.oauth2.start.bind(window.oauth2))
+        .then(function () {
+            console.log("Login good!");
+        })
+        .then(function () {
+            // TODO handle user_id response data?
+            return Promise.resolve($.ajax({
+                type: 'GET',
+                url: 'https://api.mercadolibre.com/users/me?access_token=' + window.oauth2.getAuth().token,
+                success: function success(data) {
+                    console.log("login success! ", data);
+                }
+            }));
+        })
+        .then(loginSuccess)
+        .catch(function (err) {
+            console.error("Login bad: " + err.stack);
+        })
+        .finally(function () {
+            waitMe.stop();
+        });
 }
 
 function configureLogin() {
-  window.oauth2.options.full_url = "http://auth.mercadolibre.com.ar/authorization?response_type=token&client_id=3791482542047777";
-  window.oauth2.options.user_id = "user_id";
+    window.oauth2.options.full_url = "http://auth.mercadolibre.com.ar/authorization?response_type=token&client_id=3791482542047777";
+    window.oauth2.options.user_id = "user_id";
 }
 
 function getMeliQuestions() {
@@ -62,18 +61,18 @@ function loginSuccess() {
 }
 
 function _checkAccessToken() {
-  var userLoggedIn = window.oauth2.checkLogin();
-  console.log("logged? ", userLoggedIn);
-  return userLoggedIn;
+    var userLoggedIn = window.oauth2.checkLogin();
+    console.log("logged? ", userLoggedIn);
+    return userLoggedIn;
 }
 
 $(document).on("ready", function () {
-  configureLogin();
+    configureLogin();
 
-  if (!_checkAccessToken()) {
-    startLogin();
-    return;
-  }
+    if (!_checkAccessToken()) {
+        startLogin();
+        return;
+    }
 
-  loginSuccess();
+    loginSuccess();
 });
