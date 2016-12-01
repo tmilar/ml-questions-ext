5f5f57e48a57a1ef6570fddec11d3355c99f229d
tmilar <tomasmilar4@gmail.com>
2016-12-01 01:58:01 -0300

    Actually compile pre-compiled hbsTemplates.hbs, and display result on popup screen

--- app/src/popup/questions/questions-module.js
+++ app/src/popup/questions/questions-module.js
@@ -11,17 +11,12 @@ var QuestionsModule = (function QuestionsModule() {
     }
 
     function load() {
+        var compiledHbs = MeliPreguntasApp.templates['questions-view'](questions);
         var target = $(".questions.content");
         var mainUrl = "src/popup/questions/html/main_test.html";
         console.log("About to load questions data.. ", questions, target);
 
-        target.load(mainUrl, function (response, status, xhr) {
-            if (status === "error") {
-                console.error("Error al cargar la vista de preguntas: " + xhr.status + " " + xhr.statusText);
-                return;
-            }
-            console.log("Vista preguntas, cargada ok!", mainUrl);
-        });
+        target.html(compiledHbs);
     }
 
     return {
