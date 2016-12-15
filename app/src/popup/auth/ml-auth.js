5e7c8a0ec59253efbc3106e16f39b0145aa922d1
tmilar <tomasmilar4@gmail.com>
2016-12-14 23:34:28 -0300

    Extract UserService from ML Auth service, to a separate user-service.js file

--- app/src/popup/auth/ml-auth.js
+++ app/src/popup/auth/ml-auth.js
@@ -125,54 +125,6 @@ var Auth = (function () {
             });
     }
 
-    var UserService = {
-        addUser: function (userInfo) {
-            var users = localStorage.get('users');
-
-            var newUserId = userInfo.user_id;
-
-            // check if id key is present
-            if (!newUserId) {
-                console.error("No user_id received! Can't register!");
-                return;
-            }
-
-            // initialize users hash if not existant, and mark new user as primary
-            if (_.isEmpty(users)) {
-                userInfo.primary = true;
-                users = {};
-            }
-
-            // check if user was already registered
-            var existingUser = users[newUserId];
-            if (existingUser) {
-                console.log("User id: ", newUserId, " was already registered! Current info: ", existingUser, ". Updating with new info: ", userInfo);
-                _.merge(existingUser, userInfo);
-            } else {
-                console.log("Registering new user: ", userInfo);
-                users[newUserId] = userInfo;
-            }
-
-            localStorage.set('users', users);
-        },
-
-        removeUser: function (userId) {
-            var users = localStorage.get('users');
-            if (!users[userId]) {
-                console.error("Error, user id ", userId, "not found! ");
-                return false;
-            }
-            delete users[userId];
-            localStorage.set('users', users);
-            return true;
-        },
-        getUsers: function () {
-            return localStorage.get('users');
-        },
-        getUsersArray: function () {
-            return _.values(this.getUsers())
-        }
-    };
 
     function init() {
         var clientId = 3791482542047777;
